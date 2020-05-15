5da07bcc9e4bf453977566ac3276e3bec6e9102a
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-05-14 18:41:32 -0600

    New Mix project

---
+++ lib/metatorrent.ex
@@ -0,0 +1,18 @@
+defmodule Metatorrent do
+  @moduledoc """
+  Documentation for Metatorrent.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Metatorrent.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metatorrent_test.exs
@@ -0,0 +1,8 @@
+defmodule MetatorrentTest do
+  use ExUnit.Case
+  doctest Metatorrent
+
+  test "greets the world" do
+    assert Metatorrent.hello() == :world
+  end
+end
